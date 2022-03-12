5fcaeee3be9ec8cb46d7a529fcee11757c7cbc8d
hugosenari <hugosenari@gmail.com>
2022-03-12 02:06:05 -0300

    chore: re index eth

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A collection of Ethereum related libraries'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."eth-master".url = "path:./master";
+  inputs."eth-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."eth-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
